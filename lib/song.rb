33faaaedb6d2fb65ac8bbe82b98f3e94dbe75375
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-21 20:06:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
     song = Song.new(song_name)
-    song.artist = artist_name
+    song.artist = Artist.find_or_create_by_name(artist_name)
     binding.pry
     @@all << song
     song
